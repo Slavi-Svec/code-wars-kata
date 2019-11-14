b3851893f4a7fc4c18ac16ac5f98bf46997880f9
izaak svec <slavi.svec@gmail.com>
2019-11-14 22:46:22 +1100

    Update codewar Kata Jenny's secret message

--- codewars.js
+++ codewars.js
@@ -50,8 +50,7 @@
 
 // answer:
 // let words = ['hello', 'world', 'this', 'is', 'great'];
-// let smash = words.join(' ');
-// console.log(smash);
+// return words.join(' ');
 
 
 // const words = ['hello', 'world', 'this', 'is', 'great'];
@@ -62,10 +61,21 @@
 
 
 // const words = ['hello', 'world', 'this', 'is', 'great'];
-let smash  = (words) => {
-   let x =  words.join(' ');
-   return x;
-};
+// const smash = words => words.join(' ')
 
 
+// console.log(smash(['hello', 'world', 'this', 'is', 'great']))
 
+
+// question
+// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, 
+// and would like to greet him slightly different. She added a special case to her function, 
+// but she made a mistake.
+
+// Can you help her?
+
+function greet(name){
+   return "Hello, " + name + "!";
+   if(name === "Johnny")
+     return "Hello, my love!";
+ }
